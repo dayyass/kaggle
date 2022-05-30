97e95e47593ebb06d91bb78e080b443b337ea0aa
Dani El-Ayyass <dayyass@yandex.ru>
2022-05-30 21:20:13 +0300

    add SiameseSigmoidBERT with several intertower poolers

--- Quora_Question_Pairs/model.py
+++ Quora_Question_Pairs/model.py
@@ -122,6 +122,115 @@ class SiameseManhattanBERT(torch.nn.Module):
         return scores
 
 
+class SiameseSigmoidBERT(torch.nn.Module):
+    """
+    Siamese Sigmoid BERT.
+    """
+
+    def __init__(
+        self,
+        bert_model: transformers.PreTrainedModel,
+        pooler: torch.nn.Module,
+        intertower_pooler: torch.nn.Module,
+    ):
+        """
+        Model initialization with BERT model and pooler.
+
+        Args:
+            bert_model (transformers.PreTrainedModel): BERT model.
+            pooler (torch.nn.Module): pooler to get embeddings from bert_model output.
+            intertower_pooler (torch.nn.Module): intertower pooler to process two towers embedding.
+        """
+
+        super().__init__()
+
+        self.bert_model = bert_model
+        self.pooler = pooler
+        self.intertower_pooler = intertower_pooler
+
+    def forward(
+        self,
+        x1: transformers.BatchEncoding,
+        x2: transformers.BatchEncoding,
+    ) -> torch.Tensor:
+        """
+        Forward pass with two tokenized sentences x1 and x2.
+
+        Args:
+            x1 (transformers.BatchEncoding): tokenized sentences.
+            x2 (transformers.BatchEncoding): tokenized sentences.
+
+        Returns:
+            torch.Tensor: semantic similarity logit.
+        """
+
+        x1 = self._vectorize(x1)
+        x2 = self._vectorize(x2)
+        return self.intertower_pooler(x1, x2)
+
+    def vectorize(
+        self,
+        texts: List[str],
+        tokenizer: transformers.PreTrainedTokenizer,
+        tokenizer_kwargs: Dict[str, int],
+    ) -> torch.Tensor:
+        """
+        Inference-time method to get text embedding.
+
+        Args:
+            texts (List[str]): list of sentences.
+            tokenizer (transformers.PreTrainedTokenizer): transformers tokenizer.
+            tokenizer_kwargs (Dict[str, int]): transformers parameters.
+
+        Returns:
+            torch.Tensor: text embedding.
+        """
+
+        device = self.bert_model.device
+
+        tokens = tokenizer(texts, **tokenizer_kwargs).to(device)
+        with torch.no_grad():
+            embedding = self._vectorize(tokens)
+
+        return embedding
+
+    def _vectorize(self, x: transformers.BatchEncoding) -> torch.Tensor:
+        """
+        Get embedding from tokenized sentences x.
+
+        Args:
+            x (transformers.BatchEncoding): tokenized sentences.
+
+        Returns:
+            torch.Tensor: embedding.
+        """
+
+        return self.pooler(self.bert_model(**x))
+
+    def similarity_sigmoid_score(
+        self,
+        x1_emb: torch.Tensor,
+        x2_emb: torch.Tensor,
+    ) -> torch.Tensor:
+        """
+        Calculate semantic similarity sigmoid scores given embedding of the sentences batches.
+
+        Args:
+            x1_emb (torch.Tensor): embeddings of sentences batch.
+            x2_emb (torch.Tensor): embeddings of sentences batch.
+
+        Returns:
+            torch.Tensor: semantic similarity sigmoid scores.
+        """
+
+        with torch.no_grad():
+            scores = self.intertower_pooler(x1_emb, x2_emb).sigmoid()
+
+        return scores
+
+
+# TOWER POOLERS
+
 class ClsPooler(torch.nn.Module):
     """
     CLS pooler.
@@ -193,3 +302,62 @@ class ConcatPooler(torch.nn.Module):
         for pooler in self.poolers:
             pool_list.append(pooler(x))
         return torch.cat(pool_list, dim=-1)
+
+
+# INTERTOWER POOLERS
+
+class IntertowerConcatPooler(torch.nn.Module):
+    """
+    Intertower concatenation pooler.
+    """
+
+    def __init__(self, model_hidden_size: int):
+        super().__init__()
+        self.model_hidden_size = model_hidden_size * 2
+        self.pooler = torch.nn.Linear(self.model_hidden_size, 1)
+
+    def _concat(self, x1_emb: torch.Tensor, x2_emb: torch.Tensor) -> torch.Tensor:
+        return torch.cat([x1_emb, x2_emb], dim=-1)
+
+    def forward(self, x1_emb: torch.Tensor, x2_emb: torch.Tensor) -> torch.Tensor:
+        concat = self._concat(x1_emb, x2_emb)
+        return self.pooler(concat).squeeze(-1)  # logit
+
+
+class IntertowerConcatPoolerWithAbsDiff(torch.nn.Module):
+    """
+    Intertower concatenation pooler with absolute difference of embeddings.
+    """
+
+    def __init__(self, model_hidden_size: int):
+        super().__init__()
+        self.model_hidden_size = model_hidden_size * 3
+        self.pooler = torch.nn.Linear(self.model_hidden_size, 1)
+
+    def _concat(self, x1_emb: torch.Tensor, x2_emb: torch.Tensor) -> torch.Tensor:
+        x3_abs_diff = torch.abs(x1_emb - x2_emb)
+        return torch.cat([x1_emb, x2_emb, x3_abs_diff], dim=-1)
+
+    def forward(self, x1_emb: torch.Tensor, x2_emb: torch.Tensor) -> torch.Tensor:
+        concat = self._concat(x1_emb, x2_emb)
+        return self.pooler(concat).squeeze(-1)  # logit
+
+
+class IntertowerConcatPoolerWithAbsDiffAndProduct(torch.nn.Module):
+    """
+    Intertower concatenation pooler with absolute difference of embeddings and element-wise product.
+    """
+
+    def __init__(self, model_hidden_size: int):
+        super().__init__()
+        self.model_hidden_size = model_hidden_size * 4
+        self.pooler = torch.nn.Linear(self.model_hidden_size, 1)
+
+    def _concat(self, x1_emb: torch.Tensor, x2_emb: torch.Tensor) -> torch.Tensor:
+        x3_abs_diff = torch.abs(x1_emb - x2_emb)
+        x4_prod = x1_emb * x2_emb
+        return torch.cat([x1_emb, x2_emb, x3_abs_diff, x4_prod], dim=-1)
+
+    def forward(self, x1_emb: torch.Tensor, x2_emb: torch.Tensor) -> torch.Tensor:
+        concat = self._concat(x1_emb, x2_emb)
+        return self.pooler(concat).squeeze(-1)  # logit
