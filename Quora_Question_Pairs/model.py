21ad3df14cf7e81a334cefdb230cb08d23c394a9
Dani El-Ayyass <dayyass@yandex.ru>
2022-05-31 16:50:33 +0300

    add model and train for contrastive training

--- Quora_Question_Pairs/model.py
+++ Quora_Question_Pairs/model.py
@@ -1,4 +1,4 @@
-from typing import Dict, List, Union
+from typing import Dict, List, Tuple, Union
 
 import numpy as np
 import torch
@@ -225,6 +225,110 @@ class SiameseSigmoidBERT(torch.nn.Module):
 
         with torch.no_grad():
             scores = self.intertower_pooler(x1_emb, x2_emb).sigmoid()
+
+        return scores
+
+
+class SiameseContrastiveBERT(torch.nn.Module):
+    """
+    Siamese BERT with Contrastive Loss.
+    """
+
+    def __init__(
+        self,
+        bert_model: transformers.PreTrainedModel,
+        pooler: torch.nn.Module,
+    ):
+        """
+        Model initialization with BERT model and pooler.
+
+        Args:
+            bert_model (transformers.PreTrainedModel): BERT model.
+            pooler (torch.nn.Module): pooler to get embeddings from bert_model output.
+        """
+
+        super().__init__()
+
+        self.bert_model = bert_model
+        self.pooler = pooler
+
+    def forward(
+        self,
+        x1: transformers.BatchEncoding,
+        x2: transformers.BatchEncoding,
+    ) -> Tuple[torch.Tensor, torch.Tensor]:
+        """
+        Forward pass with two tokenized sentences x1 and x2.
+
+        Args:
+            x1 (transformers.BatchEncoding): tokenized sentences.
+            x2 (transformers.BatchEncoding): tokenized sentences.
+
+        Returns:
+            Tuple[torch.Tensor, torch.Tensor]: x1 and x2 embeddings.
+        """
+
+        x1 = self._vectorize(x1)
+        x2 = self._vectorize(x2)
+        return x1, x2
+
+    def vectorize(
+        self,
+        texts: List[str],
+        tokenizer: transformers.PreTrainedTokenizer,
+        tokenizer_kwargs: Dict[str, int],
+    ) -> torch.Tensor:
+        """
+        Inference-time method to get text embedding.
+
+        Args:
+            texts (List[str]): list of sentences.
+            tokenizer (transformers.PreTrainedTokenizer): transformers tokenizer.
+            tokenizer_kwargs (Dict[str, int]): transformers parameters.
+
+        Returns:
+            torch.Tensor: text embedding.
+        """
+
+        device = self.bert_model.device
+
+        tokens = tokenizer(texts, **tokenizer_kwargs).to(device)
+        with torch.no_grad():
+            embedding = self._vectorize(tokens)
+
+        return embedding
+
+    def _vectorize(self, x: transformers.BatchEncoding) -> torch.Tensor:
+        """
+        Get embedding from tokenized sentences x.
+
+        Args:
+            x (transformers.BatchEncoding): tokenized sentences.
+
+        Returns:
+            torch.Tensor: embedding.
+        """
+
+        return self.pooler(self.bert_model(**x))
+
+    def similarity_euclidean_score(
+        self,
+        x1_emb: torch.Tensor,
+        x2_emb: torch.Tensor,
+    ) -> torch.Tensor:
+        """
+        Calculate semantic similarity euclidean scores given embedding of the sentences batches.
+
+        Args:
+            x1_emb (torch.Tensor): embeddings of sentences batch.
+            x2_emb (torch.Tensor): embeddings of sentences batch.
+
+        Returns:
+            torch.Tensor: semantic similarity euclidean scores.
+        """
+
+        with torch.no_grad():
+            scores = torch.nn.functional.pairwise_distance(x1_emb, x2_emb)
 
         return scores
 
