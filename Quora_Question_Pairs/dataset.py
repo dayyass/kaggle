8594ef57354bf9798bc569fecd7acc0e5c084a66
Dani El-Ayyass <dayyass@yandex.ru>
2022-06-01 16:59:41 +0300

    add triplet dataset and collator

--- Quora_Question_Pairs/dataset.py
+++ Quora_Question_Pairs/dataset.py
@@ -1,4 +1,4 @@
-from typing import Dict, Tuple
+from typing import Dict, List, Tuple
 
 import pandas as pd
 import torch
@@ -50,6 +50,57 @@ class Dataset(torch.utils.data.Dataset):
         return q1, q2, tgt
 
 
+class TripletDataset(torch.utils.data.Dataset):
+    """
+    Quora Question Pairs triplet dataset.
+    """
+
+    def __init__(self, df: pd.DataFrame, n_negative_samples: int):
+        """
+        Dataset initialization.
+
+        Args:
+            df (pd.DataFrame): Quora Question Pairs dataframe.
+        """
+
+        self.df = df
+        self.n_negative_samples = n_negative_samples
+
+    def __len__(self) -> int:
+        """
+        Dataset length.
+
+        Returns:
+            int: dataset length.
+        """
+
+        return len(self.df)
+
+    def __getitem__(self, idx: int) -> Tuple[List[str], List[str], List[str]]:
+        """
+        Get dataframe row with anchor, positive and negative questions.
+
+        Args:
+            idx (int): row index.
+
+        Returns:
+            Tuple[List[str], List[str], List[str]]: anchor, positive and negative questions.
+        """
+
+        row = self.df.iloc[idx]
+
+        anchor = row["question1"]
+        positive = row["question2"]
+        negative = self.df[
+            self.df["question1"] != anchor
+        ]["question2"].sample(self.n_negative_samples).to_list()
+
+        anchor = [anchor] * self.n_negative_samples
+        positive = [positive] * self.n_negative_samples
+
+        return anchor, positive, negative
+
+
 class Collator:
     """
     Quora Question Pairs collator that tokenizes sentences.
@@ -91,3 +142,50 @@ class Collator:
         tgt = torch.Tensor(tgt)
 
         return q1, q2, tgt
+
+
+class TripletCollator:
+    """
+    Quora Question Pairs collator that tokenizes triplets.
+    """
+
+    def __init__(
+        self,
+        tokenizer: transformers.PreTrainedTokenizer,
+        tokenizer_kwargs: Dict[str, int],
+    ):
+        """
+        Collator initialization with transformers tokenizer and its parameters.
+
+        Args:
+            tokenizer (transformers.PreTrainedTokenizer): transformers tokenizer.
+            tokenizer_kwargs (Dict[str, int]): transformers parameters.
+        """
+
+        self.tokenizer = tokenizer
+        self.tokenizer_kwargs = tokenizer_kwargs
+
+    def __call__(
+        self, batch: Tuple[List[str], List[str], List[str]]
+    ) -> Tuple[transformers.BatchEncoding, transformers.BatchEncoding, torch.Tensor]:
+        """
+        Create tokenized batch from strings.
+
+        Args:
+            batch (Tuple[List[str], List[str], List[str]]): source strings data triplets.
+
+        Returns:
+            Tuple[transformers.BatchEncoding, transformers.BatchEncoding, torch.Tensor]: tokenized batch.
+        """
+
+        anchor, positive, negative = zip(*batch)  # type: ignore
+
+        anchor = self.tokenizer(self.flatten(anchor), **self.tokenizer_kwargs)  # type: ignore
+        positive = self.tokenizer(self.flatten(positive), **self.tokenizer_kwargs)  # type: ignore
+        negative = self.tokenizer(self.flatten(negative), **self.tokenizer_kwargs)  # type: ignore
+
+        return anchor, positive, negative
+
+    @staticmethod
+    def flatten(list_of_lists: List[List[str]]) -> List[str]:
+        return [item for lst in list_of_lists for item in lst]
